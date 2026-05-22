59d23a74f3d40c73721c67946b6fc2d1c9cd90b0
fyuo863 <2357645321@qq.com>
2026-05-22 19:03:20 +0800

    feat: 完善router功能占位

---
+++ backend/internal/handler/blog.go
@@ -0,0 +1,18 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func CreateBlog(c *gin.Context) {
+}
+
+func ListBlogs(c *gin.Context) {
+}
+
+func GetBlog(c *gin.Context) {
+}
+
+func UpdateBlog(c *gin.Context) {
+}
+
+func DeleteBlog(c *gin.Context) {
+}
